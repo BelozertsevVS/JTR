e1199c2b2fb9340d9e4cbbe9969f0e574883ce1c
BelozertsevVS <bvs.wl1980@gmail.com>
2024-08-10 18:53:20 +0300

    Topic1

---
+++ src/main/java/com/ua/jtr/topic1/MainOperators.java
@@ -0,0 +1,21 @@
+package com.ua.jtr.topic1;
+
+public class MainOperators {
+    public static void main(String[] args) {
+
+        System.out.println(5 % 3);
+
+        System.out.println();
+
+        int q = 9;
+
+
+        System.out.println(q++ + ++q);
+
+
+
+
+
+
+    }
+}
